a2c1bd220ce10d335a8e6dfb6f1092f2d96cf797
koteswari6170 <102402089+koteswari6170@users.noreply.github.com>
2022-04-15 12:21:48 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,95 @@
+import math
+# print(math.ceil(0.1))#1
+# print(math.ceil(2.4))#3
+# print(math.ceil(5.3))#6
+# print(math.ceil(-5.3))#-5
+# print(math.ceil(22.6))#23
+# print(math.ceil(10.0))#10
+
+# print(math.exp(1))#2.71828
+# print(math.exp(2))#7.38905
+
+# print(math.factorial(0))#1
+# print(math.factorial(1))#1
+# print(math.factorial(2))#2
+# print(math.factorial(3))#6
+# print(math.factorial(4))#24
+# print(math.factorial(5))#120
+
+# print(math.floor(23.4))#23
+# print(math.floor(23.5))#23
+# print(math.floor(23.6))#23
+# print(math.floor(-23.6))#-24
+# print(math.floor(-19.0))#-19
+# print(math.floor(-19.1))#-20
+# print(math.ceil(7.0))#7
+# print(math.ceil(6.9))#7
+# print(math.floor(6.9))#6
+# print(math.ceil(7.5))#8
+
+# a=math.pow(3,3)
+# b=3**3
+# print(a,type(a))#27.0 float
+# print(b,type(b))#27 int
+# print(math.pi)#3.141592653589793
+# print(math.pi*2)#6.283185307179586
+# print(math.pi*3**2)#28.274333882308138
+# print(math.sqrt(4))#2.0
+# print(math.sqrt(6.7))#2.588435821108
+# print(math.sqrt(24))#4.898
+# print(math.sqrt(25))#5.0
+
+# c=2
+# d=6
+# e=14
+# f=0
+# print(math.isqrt(c))#1
+# print(math.isqrt(d))#2
+# print(math.isqrt(e))#3
+# print(math.isqrt(f))#0
+
+# v=[1,2,3,4,5]
+# print(sum(v))#15
+# print(math.fsum(v))#15.0
+# print(math.prod(v))#120
+# print(int(math.fsum(v)))#15
+
+# g=17%3
+# print(g)#2
+# v=-4
+# print(abs(v))#4
+# print(math.fabs(v))#4.0
+# print(math.cos(45))#0.5253219888177297
+# print(math.sin(45))#0.8509035245341184
+# print(math.tan(45))#1.6197751905438615
+# print(math.trunc(4.55))#4
+# print(math.trunc(-4.55))#-4
+
+# print(math.gcd(18,11))#1
+# print(math.gcd(11,18))#1
+# print(math.gcd(14,13))#1
+# print(math.gcd(17,16))#1
+# print(math.gcd(13,14))#1
+# print(math.gcd(18,17))#1
+# print(math.gcd(20,27))#1
+# print(math.gcd(34,67))#1
+# print(math.gcd(11,13))#1
+# print(math.gcd(7,9))#1
+# print(math.gcd(24,32))#8
+# print(math.gcd(38,64))#2
+# print(math.gcd(52,86))#2
+
+# print(math.lcm(6,16))#48
+# print(math.lcm(8,14))#56
+
+# import math
+# print(dir(math))#'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 
+#'ceil', 'comb', 'copysign', 'cos', 'cosh', 'degrees', 'dist', 'e',
+#  'erf', 'erfc', 'exp', 'expm1', 'fabs', 'factorial', 'floor', 'fmod',
+#  'frexp', 'fsum', 'gamma', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 
+# 'isnan', 'isqrt', 'lcm', 'ldexp', 'lgamma', 'log', 'log10', 'log1p', 'log2', 'modf',
+#  'nan', 'nextafter', 'perm', 'pi', 'pow', 'prod', 'radians',
+#  'remainder', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'tau', 'trunc', 'ulp']
+
+
+
